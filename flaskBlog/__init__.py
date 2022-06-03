c36bb0f7e8cac87bf488d9708cdfbb5036eeb8f6
Michal Matracz <michalmatracz@gmail.com>
2022-06-03 21:28:56 +0200

    Dodawnie do bazy uzytkowników sprawdzanie czy klucz sie nie powtarza

--- flaskBlog/__init__.py
+++ flaskBlog/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd6ee049fbe11f6e895bb2624748c7b93'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 from flaskBlog import routes
